26dbaba1bb608dfcbf18b671f23ea66afe966ec3
stum0 <70952638+stum0@users.noreply.github.com>
2022-11-06 15:11:23 +0000

    test fail

--- test/test.ts
+++ test/test.ts
@@ -7,7 +7,7 @@ import { equal } from 'assert';
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      equal([1, 2, 3].indexOf(10), -1);
+      equal([1, 2, 3].indexOf(1), -1);
     });
   });
 });
