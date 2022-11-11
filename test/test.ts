02aec49225444990ade6cf94040d549e1c59102f
stum0 <70952638+stum0@users.noreply.github.com>
2022-11-11 12:41:41 +0000

    docker.iotest4

--- test/test.ts
+++ test/test.ts
@@ -1,13 +1,11 @@
 /* eslint-disable no-undef */
 //mock test for deployment testing
-import { equal } from 'assert';
+import { equal } from "assert";
 
-
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
+describe("Array", function () {
+  describe("#indexOf()", function () {
+    it("should return -1 when the value is not present", function () {
       equal([1, 2, 3].indexOf(5), -1);
     });
   });
 });
-
